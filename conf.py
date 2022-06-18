a5cd418dae87e4f1eb3ddaf40ee01f9802688ec7
Cangalala <103964892+Cangalala@users.noreply.github.com>
2022-06-18 12:39:47 -0400

    Add files via upload

---
+++ conf.py
@@ -0,0 +1,5 @@
+api_hash = "2618ec2cdeb1b513eabf02f83f5a1553"
+api_id = 15745491
+bot_token = "5458792094:AAFm6hT_OjZGHfeoS8t6BJ4GoFRnhz4oSsk"
+authorized_users = [overlordox]
+admin_user = 'overlordox'
